1b47ae32caaa6bb16d252792018e2db7f80d5233
Cezary Klamra <cklamra@int4.local.snellius.surf.nl>
2024-11-26 02:23:43 +0100

    slurm job fixes

--- ancm/job.sh
+++ ancm/job.sh
@@ -4,6 +4,7 @@
 #SBATCH -t 12:00:00
 #SBATCH -p genoa
 #SBATCH --requeue
+#SBATCH --exclusive
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=cezary.klamra@student.uva.nl
 
@@ -14,7 +15,7 @@ module load Python/3.9.5-GCCcore-10.3.0
 # Create output directory on scratch
 mkdir "$TMPDIR"/runs
  
-python -m pip install -r $HOME/EGG/ancm/requirements_39.txt
+python -m pip install -r $HOME/EGG/ancm/requirements39.txt
 python -m pip install $HOME/EGG
 
 # Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
