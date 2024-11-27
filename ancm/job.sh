bc08d5db57cf32fb639d44cd1f978f3cf37d535f
Cezary Klamra <cklamra@int4.local.snellius.surf.nl>
2024-11-27 02:39:28 +0100

    update

--- ancm/job.sh
+++ ancm/job.sh
@@ -11,12 +11,19 @@
 # Loading modules
 module load 2022
 module load Python/3.9.5-GCCcore-10.3.0
- 
+
+echo "$TMPDIR"
+python -V
+which python
+python3 -V
+which python3
+
 # Create output directory on scratch
 mkdir "$TMPDIR"/runs
  
-python -m pip install -r $HOME/EGG/ancm/requirements39.txt
-python -m pip install $HOME/EGG
+# Install packages
+# python -m pip install -r $HOME/EGG/ancm/requirements39.txt
+# python -m pip install $HOME/EGG
 
 # Execute a Python program located in $HOME, that takes an input file and output directory as arguments.
 python -m ancm.run_all --output_dir "$TMPDIR"/runs --batch_size 96
--- ancm/run_all.py
+++ ancm/run_all.py
@@ -16,11 +16,11 @@ from ancm.train import main
 from egg.core import init
 
 
-random_seeds = [1]#[i+1 for i in range(5)]
+random_seeds = [i+1 for i in range(5)]
 data_seeds = [42]
-error_probs = [0.0, 0.1] #[0.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30]
+error_probs = [0.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30]
 channels = 'erasure deletion symmetric'.split()
-max_lengths = [2,3] #[2, 3, 5, 10]
+max_lengths = [2, 3, 5, 10]
 
 slr = 5e-3
 rlr = 1e-3
@@ -54,7 +54,7 @@ def get_opts(error_prob, channel, max_len, random_seed, data_seed, filename, res
         '--perceptual_dimensions [2]*8',
         '--sender_embedding 10',
         '--receiver_embedding 10',
-        '--n_epochs 2',
+        '--n_epochs 20',
         '--sender_entropy_coeff 0.01',
         '--receiver_entropy_coeff 0.001',
         '--sender_cell lstm',
