61e204850c4c6184a4e6590a6c0a4f470d01f006
Francijn Keur <97666754+francijnk@users.noreply.github.com>
2025-02-06 08:42:56 +0100

    fixes

--- ancm/run_all.py
+++ ancm/run_all.py
@@ -5,17 +5,17 @@ from datetime import timedelta
 import subprocess
 from uuid import uuid4
 
-random_seeds = [i + 1 for i in range(5)]
-error_probs = [0.00, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30]
-channels = [None, 'erasure', 'deletion', 'symmetric']
-max_lengths = [2,3,4,5,8]
+random_seeds = [i + 1 for i in range(3)]
+error_probs = [0.00, 0.05, 0.10, 0.15, 0.20, 0.25]
+channels = ['erasure', 'symmetric']
+max_lengths = [2,3,5]
 
 slr = 5e-3
 rlr = 1e-3
 length_cost = 0.01
 vocab_size = 10
 hidden_units = 128
-n_epochs = 20
+n_epochs = 25
 emb = 16
 tau = 2.
 
@@ -31,7 +31,7 @@ for channel in channels:
                 print(f"seed: {seed}")
                 print(f"max_len: {max_len}")
 
-                filename = f'{channel}_{max_len}_{seed}'
+                filename = f'{channel}_{max_len}_{seed}_{pr}'
             
                 opts = [
                     f'--error_prob {pr:.2f}',
@@ -60,15 +60,15 @@ for channel in channels:
                     '--wandb_project cezary_snellius ',
                     '--wandb_entity koala-lab',
                     f'--wandb_run_id {max_len}_{channel}_{seed}_{uuid4()}',
-                    '--results_folder C:/Users/franc/Documents/GitHub/ANCM_EGG/ancm/runs/results_02_05_img',
+                    '--results_folder /content/drive/MyDrive/MoL/ANCM',
+                    f'--channel {channel}',
+                    
                 ]
-
-                if channel is not None:
-                    opts.append(f'--channel {channel}')
 
                 process = subprocess.Popen(
                     ['python3', '-m', 'ancm.train']
                     + [o for opt in opts[:-1] for o in opt.split()]
+                    + []
                     )
                 exitcode = process.wait()
 
@@ -82,7 +82,7 @@ for channel in channels:
                 run_count += 1
  
 training_time = timedelta(seconds=time.monotonic()-t_start)
-sec_per_run = training_time.seconds / (len(random_seeds) * len(max_lengths) * len(error_probs))
+sec_per_run = training_time.seconds / (len(random_seeds) * len(max_lengths) * len(error_probs) * len(channels))
 minutes, seconds = divmod(sec_per_run, 60)
 
 time_total = str(training_time).split('.', maxsplit=1)[0]
@@ -91,3 +91,7 @@ time_per_run = f'{int(minutes):02}:{int(seconds):02}'
 print("Total training time:", time_total)
 print("       Time per run:", time_per_run)
 
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
